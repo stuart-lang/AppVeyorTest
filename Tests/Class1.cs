52970f12d8e40935ae4c32f6c6d5a1fb36c43b93
Stuart Lang <stuart.lang@just-eat.com>
2016-04-03 19:37:16 +0100

    Logged parent proc

--- Tests/Class1.cs
+++ Tests/Class1.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Management;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
@@ -19,6 +21,8 @@ namespace Tests
                 .ToList()
                 .ForEach(Console.WriteLine);
 
+            Console.WriteLine(Process.GetCurrentProcess().Parent().GetCommandLine());
+
             Console.WriteLine("Environment variables:");
             Environment.GetEnvironmentVariables()
                 .Cast<DictionaryEntry>()
@@ -26,6 +30,55 @@ namespace Tests
                 .ForEach(x => Console.WriteLine("{0}={1}", x.Key, x.Value));
 
             Assert.Pass();
+        }
+    }
+
+    public static class ProcessExtensions
+    {
+        public static string GetCommandLine(this Process process)
+        {
+            var commandLine = new StringBuilder(process.MainModule.FileName);
+
+            commandLine.Append(" ");
+            using (var searcher = new ManagementObjectSearcher("SELECT CommandLine FROM Win32_Process WHERE ProcessId = " + process.Id))
+            {
+                foreach (var @object in searcher.Get())
+                {
+                    commandLine.Append(@object["CommandLine"]);
+                    commandLine.Append(" ");
+                }
+            }
+
+            return commandLine.ToString();
+        }
+        private static string FindIndexedProcessName(int pid)
+        {
+            var processName = Process.GetProcessById(pid).ProcessName;
+            var processesByName = Process.GetProcessesByName(processName);
+            string processIndexdName = null;
+
+            for (var index = 0; index < processesByName.Length; index++)
+            {
+                processIndexdName = index == 0 ? processName : processName + "#" + index;
+                var processId = new PerformanceCounter("Process", "ID Process", processIndexdName);
+                if ((int)processId.NextValue() == pid)
+                {
+                    return processIndexdName;
+                }
+            }
+
+            return processIndexdName;
+        }
+
+        private static Process FindPidFromIndexedProcessName(string indexedProcessName)
+        {
+            var parentId = new PerformanceCounter("Process", "Creating Process ID", indexedProcessName);
+            return Process.GetProcessById((int)parentId.NextValue());
+        }
+
+        public static Process Parent(this Process process)
+        {
+            return FindPidFromIndexedProcessName(FindIndexedProcessName(process.Id));
         }
     }
 }
